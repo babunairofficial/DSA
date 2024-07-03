5b8e8a273899dca249e5bc1eae9b79ee10ff2d5d
Babu Nair <babunairofficial@gmail.com>
2024-07-03 19:57:12 +0530

    DSA - print syntax types

---
+++ Java Programming Language/javaBasics.java
@@ -0,0 +1,18 @@
+//boilerplate code
+public class javaBasics{
+    public static void main(String args[]){
+        
+        //print
+
+        System.out.print("Hello World");
+        System.out.print("Hello World");
+
+        //println
+
+        System.out.println("Hello world in new line");        
+
+        //"\n"
+
+        System.out.print("Hello World\nHello \n World");
+    }
+}
