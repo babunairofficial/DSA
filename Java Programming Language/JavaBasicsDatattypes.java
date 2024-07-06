b44e01bfb5d7de73d19360f5a88dceb1eda285fb
Babu Nair <babunairofficial@gmail.com>
2024-07-06 17:14:22 +0530

    DSA - datatypes

---
+++ Java Programming Language/JavaBasicsDatattypes.java
@@ -0,0 +1,24 @@
+public class JavaBasicsDatattypes {
+    public static void main(String args[]){
+        byte b = 8;
+        System.out.println(b);
+
+        char ch = 'a';
+        System.out.println(ch);
+
+        boolean var = true;
+        System.out.println(var);
+
+        float price = 12.1f;
+        System.out.println(price);
+
+        int number = 33;
+        System.out.println(number);
+
+        //long
+        //double
+
+        short n = 270;
+        System.out.println(n);
+    }
+}
