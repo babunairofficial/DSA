4ead09c5c07578fa8507a8b0f75e8383a4f94a97
Babu Nair <babunairofficial@gmail.com>
2024-07-06 17:21:54 +0530

    DSA - comments

--- Java Programming Language/JavaBasicsSum.java
+++ Java Programming Language/JavaBasicsSum.java
@@ -1,8 +1,17 @@
 public class JavaBasicsSum {
     public static void main(String args[]){
+        //variables
+        /*two variables declared
+          a
+          b
+         */
         int a = 8;
         int b = 7;
+
+        //sum of variables
         int sum = a+b;
+
+        //print sum
         System.out.println(sum);
     }
 }
