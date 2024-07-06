3cb782f22b47d1d58eb3da4599d77bb909f09d6a
Babu Nair <babunairofficial@gmail.com>
2024-07-06 17:05:12 +0530

    DSA - variables

---
+++ Java Programming Language/JavaBasicsVariable.java
@@ -0,0 +1,18 @@
+public class JavaBasicsVariable {
+    public static void main(String args[]){
+        int a = 10;
+        int b = 2;
+
+        System.out.println(a);
+        System.out.println(b);
+
+        String name = "Shaktimaan";
+        System.out.println(name);
+
+        a = 64;
+        System.out.println(a);
+
+        a = b;
+        System.out.println(a);
+    }
+}
