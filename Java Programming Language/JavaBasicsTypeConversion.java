0d032d0756015c35fbf17517de06d2e86080da78
Babu Nair <babunairofficial@gmail.com>
2024-07-08 21:20:23 +0530

    DSA - type conversion

---
+++ Java Programming Language/JavaBasicsTypeConversion.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class JavaBasicsTypeConversion{
+    public static void main(String args[]){
+        //int a = 25;
+        //long b = a;
+
+        int a = 25;
+        long b = a;
+        System.out.println(b);
+    }
+}
