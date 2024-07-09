23955dee612750e423109fe7954df28da16fccee
Babu Nair <babunairofficial@gmail.com>
2024-07-09 23:33:51 +0530

    DSA - type casting

---
+++ Java Programming Language/JavaBasicsTypeCasting.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class JavaBasicsTypeCasting {
+    public static void main(String args[]){
+        try (Scanner sc = new Scanner(System.in)) {
+        }
+        float number = 99.999f;
+        int number2 = (int) number;
+        System.out.println(number2);
+    }
+}
