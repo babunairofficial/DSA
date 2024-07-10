33470e889f485fffec9af9fbbb8825e68601a18d
Babu Nair <babunairofficial@gmail.com>
2024-07-10 20:31:16 +0530

    DSA - type promotion

--- Java Programming Language/JavaBasicsTypePromotions.java
+++ Java Programming Language/JavaBasicsTypePromotions.java
@@ -1,10 +1,17 @@
 public class JavaBasicsTypePromotions {
     public static void main(String args[]){
-        char a = 'a';
-        char b = 'b';
-        System.out.println("b =  "+(int)(b));
-        System.out.println("a =  "+(int)a);
-        System.out.println(a); //no type promotions
-        System.out.println(b-a); //type promotion happens for operators
+        // char a = 'a';
+        // char b = 'b';
+        // System.out.println("b =  "+(int)(b));
+        // System.out.println("a =  "+(int)a);
+        // System.out.println(a); //no type promotions
+        // System.out.println(b-a); //type promotion happens for operators
+
+        int a = 10;
+        float b = 20.25f;
+        long c = 25;
+        double d = 30;
+        double ans = a + b + c + d;
+        System.out.println(ans);
     }
 }
