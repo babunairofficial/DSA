b8a68b3062b8fa5b6cd23bfc75942e744e09f81f
Babu Nair <babunairofficial@gmail.com>
2024-07-11 23:31:48 +0530

    DSA - type promotion

--- Java Programming Language/JavaBasicsTypePromotions.java
+++ Java Programming Language/JavaBasicsTypePromotions.java
@@ -7,11 +7,16 @@ public class JavaBasicsTypePromotions {
         // System.out.println(a); //no type promotions
         // System.out.println(b-a); //type promotion happens for operators
 
-        int a = 10;
-        float b = 20.25f;
-        long c = 25;
-        double d = 30;
-        double ans = a + b + c + d;
-        System.out.println(ans);
+        // int a = 10;
+        // float b = 20.25f;
+        // long c = 25;
+        // double d = 30;
+        // double ans = a + b + c + d;
+        // System.out.println(ans);
+
+        byte b = 5;
+        // b = b * 2; //this cannot be used an an operation '*' gives resultant as integer which cannot be stored in variable which is a byte
+        byte a = (byte) (b*2); //typecasting done as an operation '*' is performed making the result an integer 
+        System.out.println(a);
     }
 }
