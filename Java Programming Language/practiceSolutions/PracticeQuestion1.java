3e666fd1e370da7c2ebc50dc07f9704691d34873
Babu Nair <babunairofficial@gmail.com>
2024-07-12 23:30:07 +0530

    DSA - practicequestion1

--- Java Programming Language/practiceSolutions/PracticeQuestion1.java
+++ Java Programming Language/practiceSolutions/PracticeQuestion1.java
@@ -18,12 +18,32 @@ public class PracticeQuestion1 {
         // System.out.println("average of 3 numbers is "+avg);
 
         //Question 2 
-        System.out.println("enter the side of a square");
-        int side = sc.nextInt();
+        // System.out.println("enter the side of a square");
+        // int side = sc.nextInt();
 
-        int area = side * side;
-        System.out.println("area of square is "+area);
+        // int area = side * side;
+        // System.out.println("area of square is "+area);
 
+        //Question 3
+        System.out.println("enter cost of pencil");
+        float pencil = sc.nextFloat();
+        System.out.println("enter cost of pen");
+        float pen = sc.nextFloat();
+        System.out.println("enter cost of eraser");
+        float eraser = sc.nextFloat();
+
+        float totalCost = pencil + pen + eraser;
+        float tax = totalCost * 0.18f;
+        float finalCost = totalCost + tax;
+
+        System.out.println("Cost of Pencil = "+pencil);
+        System.out.println("Cost of Pen = "+pen);
+        System.out.println("Cost of Eraser = "+eraser);
+        System.out.println("--------------------------------------------------------------------");
+        System.out.println("Toal Cost = "+totalCost);
+        System.out.println("Tax at 18% = "+tax);
+        System.out.println("Final Cost = "+finalCost);
+        System.out.println("You Need to pay Rupees "+finalCost+" . Thank you!");
         sc.close();
     }
 }
