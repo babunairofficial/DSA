c5b224b6d9a54bcaf4caf7a1093cf8676c2a02bb
Babu Nair <babunairofficial@gmail.com>
2024-07-12 23:18:46 +0530

    DSA - practicequestion1

--- Java Programming Language/practiceSolutions/PracticeQuestion1.java
+++ Java Programming Language/practiceSolutions/PracticeQuestion1.java
@@ -6,16 +6,23 @@ public class PracticeQuestion1 {
 
         //Question 1
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number ");
-        int a = sc.nextInt();
-        System.out.println("enter 2nd number");
-        int b = sc.nextInt();
-        System.out.println("enter 3rd number");
-        int c = sc.nextInt();
+        // System.out.println("Enter a number ");
+        // int a = sc.nextInt();
+        // System.out.println("enter 2nd number");
+        // int b = sc.nextInt();
+        // System.out.println("enter 3rd number");
+        // int c = sc.nextInt();
 
-        int sum = a + b + c;
-        int avg = sum/3;
-        System.out.println("average of 3 numbers is "+avg);
+        // int sum = a + b + c;
+        // int avg = sum/3;
+        // System.out.println("average of 3 numbers is "+avg);
+
+        //Question 2 
+        System.out.println("enter the side of a square");
+        int side = sc.nextInt();
+
+        int area = side * side;
+        System.out.println("area of square is "+area);
 
         sc.close();
     }
