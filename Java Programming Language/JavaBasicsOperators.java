9352adf24a40123e2d38078b8c5854659132ad38
Babu Nair <babunairofficial@gmail.com>
2024-07-15 23:06:00 +0530

    DSA - operators : arithmetic operators

---
+++ Java Programming Language/JavaBasicsOperators.java
@@ -0,0 +1,14 @@
+public class JavaBasicsOperators {
+    public static void main(String args[]){
+        int A = 10;
+        int B = 5;
+        
+        //Arithmetic operators
+        System.out.println("add = "+(A+B));
+        System.out.println("Subtract = "+(A-B));
+        System.out.println("multiply = "+(A*B));
+        System.out.println("divide = "+(A/B));
+        System.out.println("modulo(remainder) = "+(A%B));
+
+    }
+}
