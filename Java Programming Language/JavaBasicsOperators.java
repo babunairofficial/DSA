05d0d55cf30b9cc0e84f6d8a3948e3d207300576
Babu Nair <babunairofficial@gmail.com>
2024-07-18 20:59:32 +0530

    DSA - operators : logical operators

--- Java Programming Language/JavaBasicsOperators.java
+++ Java Programming Language/JavaBasicsOperators.java
@@ -1,7 +1,8 @@
 public class JavaBasicsOperators {
+    @SuppressWarnings("unused")
     public static void main(String args[]){
-        int A = 10;
-        int B = 5;
+        // int A = 10;
+        // int B = 5;
         
         //Arithmetic operators
         // System.out.println("add = "+(A+B));
@@ -17,11 +18,16 @@ public class JavaBasicsOperators {
         // System.out.println("decrement "+D);
 
         //relational operator
-        System.out.println("Equals to : " + (A == B));
-        System.out.println("Not Equals to : " + (A != B));
-        System.out.println("Greater than : " + (A > B));
-        System.out.println("Lesser than : " + (A < B));
-        System.out.println("Greater than or equal to : " + (A >= B));
-        System.out.println("Lesser than or equal to : f" + (A <= B));
+        // System.out.println("Equals to : " + (A == B));
+        // System.out.println("Not Equals to : " + (A != B));
+        // System.out.println("Greater than : " + (A > B));
+        // System.out.println("Lesser than : " + (A < B));
+        // System.out.println("Greater than or equal to : " + (A >= B));
+        // System.out.println("Lesser than or equal to : f" + (A <= B));
+
+        //logical operators
+        System.out.println((3<2) && (5<0));
+        System.out.println((3>2) || (5>0));
+        System.out.println(!(0>5));
     }
 }
