a1d5906f97e3f9d274705bd9c822e3ecb1d03511
Babu Nair <babunairofficial@gmail.com>
2024-07-19 23:20:58 +0530

    DSA - practicequestion2

---
+++ Java Programming Language/practiceSolutions/PracticeQuestion2.java
@@ -0,0 +1,14 @@
+package practiceSolutions;
+
+public class PracticeQuestion2 {
+    public static void main(String[] args){
+        //question 1
+        int x=2,y=5;
+
+        int exp1 = (x*y/x);
+        int exp2 = (x*(x/y));
+
+        System.out.print(exp1 + " , ");
+        System.out.print(exp2);       
+    }    
+}
