fba345472bebf97ffdb4ad39d862e07953b8984e
Babu Nair <babunairofficial@gmail.com>
2024-07-21 23:16:56 +0530

    DSA - practicequestion2

--- Java Programming Language/practiceSolutions/PracticeQuestion2.java
+++ Java Programming Language/practiceSolutions/PracticeQuestion2.java
@@ -24,11 +24,17 @@ public class PracticeQuestion2 {
         // }
 
         //question 3
-        int x, y, z;
-        x = y = z = 2;
-        x +=y;
-        y -=z;
-        z /=(x+y);
-        System.out.println(x + " " + y + " " + z);
+        // int x, y, z;
+        // x = y = z = 2;
+        // x +=y;
+        // y -=z;
+        // z /=(x+y);
+        // System.out.println(x + " " + y + " " + z);
+
+        //question 4
+        int x = 9, y = 12;
+        int a = 2, b = 4, c = 6;
+        int exp = 4/3 * (x + 34) + 9 * (a + b * c) + (3 + y * (2 + a)) / (a + b * y);
+        System.out.println(exp);
     }    
 }
