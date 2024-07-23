b8a77b8caa97fc156961fd04a1d7b8132a698cbc
Babu Nair <babunairofficial@gmail.com>
2024-07-23 23:21:06 +0530

    DSA - conditional statements- largest of 2 numbers

--- Java Programming Language/JavaBasicsConditionalStatements.java
+++ Java Programming Language/JavaBasicsConditionalStatements.java
@@ -1,15 +1,26 @@
 public class JavaBasicsConditionalStatements {
     public static void main(String[] args) {
-        int age = 16;
-        if(age >= 18){
-            System.out.println("adult : drive, vote");
-        }
+        // int age = 16;
+        // if(age >= 18){
+        //     System.out.println("adult : drive, vote");
+        // }
 
-        if (age > 13 && age < 18){
-            System.out.println("teenager");
+        // if (age > 13 && age < 18){
+        //     System.out.println("teenager");
+        // }
+        // else{
+        //     System.out.println("not adult");
+        // }
+
+        //print the largest of 2 numbers
+        int a = 1;
+        int b = 5;
+
+        if(a>=b){
+            System.out.println("a is larger");
         }
         else{
-            System.out.println("not adult");
+            System.out.println("b is larger");
         }
     }
 }
