22914c63d94373dafd0c03dce64d2cad65f2900b
Babu Nair <babunairofficial@gmail.com>
2024-07-23 23:24:08 +0530

    DSA - conditional statements- even or odd

--- Java Programming Language/JavaBasicsConditionalStatements.java
+++ Java Programming Language/JavaBasicsConditionalStatements.java
@@ -13,14 +13,23 @@ public class JavaBasicsConditionalStatements {
         // }
 
         //print the largest of 2 numbers
-        int a = 1;
-        int b = 5;
+        // int a = 1;
+        // int b = 5;
 
-        if(a>=b){
-            System.out.println("a is larger");
+        // if(a>=b){
+        //     System.out.println("a is larger");
+        // }
+        // else{
+        //     System.out.println("b is larger");
+        // }
+
+        //print if a number is odd or even
+        int x = 5;
+        if(x%2==0){
+            System.out.println("even");
         }
         else{
-            System.out.println("b is larger");
+            System.out.println("odd");
         }
     }
 }
