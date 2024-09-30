4115a299ee0f601bed685d2eb69e105f88faf147
Babu Nair <babunairofficial@gmail.com>
2024-09-30 22:33:36 +0530

    DSA - practicequestion3

--- Java Programming Language/practiceSolutions/PracticeQuestion3.java
+++ Java Programming Language/practiceSolutions/PracticeQuestion3.java
@@ -4,6 +4,7 @@ public class PracticeQuestion3 {
     public static void main(String[] args){
         
         //question1
+        /* 
         Scanner sc = new Scanner(System.in);
         
         System.out.println("enter a number");
@@ -13,7 +14,18 @@ public class PracticeQuestion3 {
         } else{
             System.out.println("the number is negative");
         }
-        
+        */
+
+        //question 2 - Print you have a fever if your temperature is above 100 and otherwise print you dont have a fever
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your fever");
+        double temperature = sc.nextDouble();
+        if(temperature > 100){
+            System.out.println("You have a fever");
+        }else{
+            System.out.println("You dont have a fever");
+        }
+
         sc.close();
     }
 }
