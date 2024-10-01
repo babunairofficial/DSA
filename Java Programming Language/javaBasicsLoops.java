75645b3a80ec9b7b0c35707ae3d7a32f7d593417
Babu Nair <babunairofficial@gmail.com>
2024-10-01 20:59:52 +0530

    DSA - loops - while

--- Java Programming Language/javaBasicsLoops.java
+++ Java Programming Language/javaBasicsLoops.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class javaBasicsLoops {
     public static void main(String[] args) {
         //while loop
@@ -10,11 +12,25 @@ public class javaBasicsLoops {
         */
 
         //while loop - print numbers from 1 to 10
+        /* 
         int n = 1;
         while(n<=10){
             System.out.print(n+" ");
             n++;
         }
+        */
+
+        //while loop - print numbers from 1 to n
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a last number");
+        int lastNumber = sc.nextInt();
+        int x = 1;
+        while(x <= lastNumber){
+            System.out.print(x + " ");
+            x++;
+        }
+        sc.close();
+
 
     }
 }
