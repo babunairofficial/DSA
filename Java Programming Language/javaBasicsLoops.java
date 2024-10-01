499638fcd4961d5d08b2daf21131b6f894361962
Babu Nair <babunairofficial@gmail.com>
2024-10-01 12:40:50 +0530

    DSA - loops - while

---
+++ Java Programming Language/javaBasicsLoops.java
@@ -0,0 +1,10 @@
+public class javaBasicsLoops {
+    public static void main(String[] args) {
+        //while loop
+        int ctr = 0;
+        while(ctr < 100){
+            System.out.println("hello world using while loop");
+            ctr++;
+        }
+    }
+}
