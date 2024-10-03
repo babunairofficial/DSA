7d814877bde0912b43be99b682a36aa6e066e712
Babu Nair <babunairofficial@gmail.com>
2024-10-03 22:13:21 +0530

    DSA - practicequestion4

--- Java Programming Language/practiceSolutions/PracticeQuestion4.java
+++ Java Programming Language/practiceSolutions/PracticeQuestion4.java
@@ -13,6 +13,7 @@ public class PracticeQuestion4 {
         */
 
         //Question 2 - Write a program that reads a set of integers, and then prints the sum of the even and odd integers.
+        /* 
         Scanner sc = new Scanner(System.in);
         
         int odd = 0;
@@ -38,6 +39,17 @@ public class PracticeQuestion4 {
         System.out.println("Sum of odd numbers is "+odd);
         
         sc.close();
+        */
 
+        //question 3 - write a program to find the factorial of any number entered by the user.
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int number = sc.nextInt();
+        int factorial=1;
+        for(int i=1; i<=number; i++){
+            factorial *= i;
+        }
+        System.out.println("Factorial of "+number+" is "+factorial);
+        sc.close();
     }
 }
