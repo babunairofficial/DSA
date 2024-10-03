4f5c2f97edaef0266930c261b7b6f0ae2d0b5c73
Babu Nair <babunairofficial@gmail.com>
2024-10-03 23:26:04 +0530

    DSA - patterns - inverted star pattern

--- Java Programming Language/javaBasicsPatterns.java
+++ Java Programming Language/javaBasicsPatterns.java
@@ -3,12 +3,26 @@ import java.util.Scanner;
 public class javaBasicsPatterns {
     public static void main(String[] args) {
         //Nested loop (Star pattern)
+        /* 
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number");
         int star = sc.nextInt();
         for(int i = 1; i<=star; i++){
             for(int j=1; j<=i; j++){
                 System.out.print("* ");
+            }
+            System.out.println();
+        }
+        sc.close();
+        */
+
+        //Inverted Star Pattern
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int star = sc.nextInt();
+        for(int i = 0; i<=star; i++){
+            for(int j=1; j<=star-i; j++){
+                System.out.print("* ");                
             }
             System.out.println();
         }
