90a1a559c784f98baa99ea566902e07b4daa7f72
Babu Nair <babunairofficial@gmail.com>
2024-10-03 23:31:11 +0530

    DSA - patterns - half pyramid pattern

--- Java Programming Language/javaBasicsPatterns.java
+++ Java Programming Language/javaBasicsPatterns.java
@@ -17,12 +17,26 @@ public class javaBasicsPatterns {
         */
 
         //Inverted Star Pattern
+        /* 
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number");
         int star = sc.nextInt();
         for(int i = 0; i<=star; i++){
             for(int j=1; j<=star-i; j++){
                 System.out.print("* ");                
+            }
+            System.out.println();
+        }
+        sc.close();
+        */
+
+        //Half Pyramid Pattern
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int star = sc.nextInt();
+        for(int i = 1; i<=star; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(j+" ");
             }
             System.out.println();
         }
