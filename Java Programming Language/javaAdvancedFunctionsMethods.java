69660029aa8ace66a727e72d64044d11417db15b
Babu Nair <babunairofficial@gmail.com>
2024-10-04 11:30:46 +0530

    DSA - Functions/Methods - functions with arguments

--- Java Programming Language/javaAdvancedFunctionsMethods.java
+++ Java Programming Language/javaAdvancedFunctionsMethods.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class javaAdvancedFunctionsMethods {
     public static void introduce(){
         System.out.println("Hello");
         System.out.println("I am Babu");
         System.out.println("I love java programming");
     }
+    public static int calculateSum(int num1, int num2){
+        int sum = num1 + num2;
+        return sum;
+    }
     public static void main(String[] args) {
         introduce();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter first number");
+        int a = sc.nextInt();
+        System.out.println("enter second number");
+        int b = sc.nextInt();
+
+        int sum = calculateSum(a, b); //this sum is different from the sum in calculateSum function
+        System.out.println("the sum is "+sum);
+        sc.close();
+
     }
 }
