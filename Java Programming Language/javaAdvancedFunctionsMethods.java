a0eaaa9f10aa550ecc3def7fdb002416ec06b93f
Babu Nair <babunairofficial@gmail.com>
2024-10-04 19:10:38 +0530

    DSA - Functions/Methods - find binomial coefficient

--- Java Programming Language/javaAdvancedFunctionsMethods.java
+++ Java Programming Language/javaAdvancedFunctionsMethods.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class javaAdvancedFunctionsMethods {
+    /* 
     public static void introduce(){
         System.out.println("Hello");
         System.out.println("I am Babu");
@@ -21,6 +22,35 @@ public class javaAdvancedFunctionsMethods {
         int sum = calculateSum(a, b); //this sum is different from the sum in calculateSum function
         System.out.println("the sum is "+sum);
         sc.close();
+    
+    }
+    */
+    //functions - find binomial coefficient
 
+    //find factorial
+    public static int factorial(int a){
+        int fact = 1;
+        for(int i=1; i<=a;i++){
+            fact *= i;
+        }
+        return fact;
+    }
+    //find bincoeff
+    public static int bincoeff(int n, int r){
+        int fact_n = factorial(n);
+        int fact_r = factorial(r);
+        int fact_nmr = factorial(n-r);
+        int bincoeff = fact_n/(fact_r*fact_nmr);
+        return bincoeff;
+    }
+    //arguments in main
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n ");
+        int n = sc.nextInt();
+        System.out.println("enter the value of r ");
+        int r = sc.nextInt();
+        System.out.println("The binomial coefficient is "+bincoeff(n, r));
+        sc.close();
     }
 }
