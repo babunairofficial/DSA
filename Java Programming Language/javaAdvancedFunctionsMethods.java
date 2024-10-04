523bd3b3dc2db9669dc9b8c949ee1d95309e2e5f
Babu Nair <babunairofficial@gmail.com>
2024-10-04 11:22:08 +0530

    DSA - Functions/Methods

---
+++ Java Programming Language/javaAdvancedFunctionsMethods.java
@@ -0,0 +1,10 @@
+public class javaAdvancedFunctionsMethods {
+    public static void introduce(){
+        System.out.println("Hello");
+        System.out.println("I am Babu");
+        System.out.println("I love java programming");
+    }
+    public static void main(String[] args) {
+        introduce();
+    }
+}
