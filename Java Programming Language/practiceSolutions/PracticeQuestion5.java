84f832e8c4ebfc462749e1aa1e3ac513dba31ea4
Babu Nair <babunairofficial@gmail.com>
2024-10-07 22:05:51 +0530

    DSA - practicequestion5

--- Java Programming Language/practiceSolutions/PracticeQuestion5.java
+++ Java Programming Language/practiceSolutions/PracticeQuestion5.java
@@ -3,7 +3,7 @@ package practiceSolutions;
 import java.util.Scanner;
 
 public class PracticeQuestion5 {
-    //Question 1 - java method to compute the average of three numbers.
+    /*//Question 1 - java method to compute the average of three numbers.
     public static double avg(int a, int b, int c){
         return (double)(a+b+c)/3;
     }
@@ -17,6 +17,24 @@ public class PracticeQuestion5 {
         int c = sc.nextInt();
 
         System.out.println("average of "+a+", "+b+", "+c+" is "+avg(a,b,c));
+
+        sc.close();
+    }
+    */
+
+    //Question 2 - a method to find if a number is even or not. 
+    public static boolean isEven(int n){
+        if(n%2 == 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter 1st number");
+        int n = sc.nextInt();
+        System.out.println("Is "+n+" even? "+isEven(n));
 
         sc.close();
     }
