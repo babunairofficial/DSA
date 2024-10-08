2ddd9f2388638ff414af93afb482bcbe52786957
Babu Nair <babunairofficial@gmail.com>
2024-10-08 22:40:22 +0530

    DSA - pattern 2 - hollow rectangle

---
+++ Java Programming Language/javaAdvancedPatterns2.java
@@ -0,0 +1,20 @@
+public class javaAdvancedPatterns2 {
+    
+    //Hollow Rectangle Pattern
+    public static void hollow_Rectangle(int rows, int cols){
+        for(int i = 1; i <= rows; i++){
+            for(int j = 1; j<=cols; j++){
+                if(i == 1 || i == rows || j == 1 || j == cols){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("");
+        }
+    }
+    public static void main(String[] args) {
+        hollow_Rectangle(5,10);
+    }
+    
+}
