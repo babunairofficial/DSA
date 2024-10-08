61fd1d4b7163db5d9df01ccc4db37a91086b289e
Babu Nair <babunairofficial@gmail.com>
2024-10-08 23:00:30 +0530

    DSA - pattern 2 - inverted half pyramid with numbers

--- Java Programming Language/javaAdvancedPatterns2.java
+++ Java Programming Language/javaAdvancedPatterns2.java
@@ -26,9 +26,20 @@ public class javaAdvancedPatterns2 {
             System.out.println("");
         }
     }
+
+    //Inverted Half Pyramid with numbers
+    public static void inverted_half_pyramid_with_numbers(int x){
+        for(int i = 1; i <= x; i++){
+            for(int j = 1; j <= x-i+1; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println("");
+        }
+    }
     public static void main(String[] args) {
         hollow_Rectangle(5,10);
         inverted_rotated_half_pyramid(5);
+        inverted_half_pyramid_with_numbers(5);
     }
     
 }
