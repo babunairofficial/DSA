6749e531459fa82424f7ee9b11e8176b4987ab81
Babu Nair <babunairofficial@gmail.com>
2024-10-10 23:53:51 +0530

    DSA - arrays part 1 - printing subarrays

--- Java Programming Language/javaAdvancedArrays1.java
+++ Java Programming Language/javaAdvancedArrays1.java
@@ -117,6 +117,7 @@ public class javaAdvancedArrays1 {
     }
     */
 
+    /* 
     //Forming Pairs in array
     public static void pairs(int numbers[]){
         for(int i = 0; i < numbers.length; i++){
@@ -129,5 +130,25 @@ public class javaAdvancedArrays1 {
     public static void main(String[] args) {
         int numbers[] = {0, 2, 4, 6, 8, 10};
         pairs(numbers);
+    }
+    */
+
+    //Print Subarrays
+    public static void subArrays(int numbers[]){
+        for(int i = 0; i < numbers.length; i++){
+            int start = i;
+            for(int j = i; j < numbers.length; j++ ){
+                int end = j;
+                for(int k = start; k <= end; k++){
+                    System.out.print(numbers[k]+" ");
+                }
+                System.out.println();                
+            }
+            System.out.println();
+        }        
+    }
+    public static void main(String[] args) {
+        int numbers[] = {0, 2, 4, 6, 8, 10};
+        subArrays(numbers);
     }
 }
