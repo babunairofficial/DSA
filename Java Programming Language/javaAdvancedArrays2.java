592bbeec42f78ec9f1e5910e025c25f731a836fa
Babu Nair <babunairofficial@gmail.com>
2024-10-11 21:42:30 +0530

    DSA - arrays part 2 - trapped water

--- Java Programming Language/javaAdvancedArrays2.java
+++ Java Programming Language/javaAdvancedArrays2.java
@@ -57,6 +57,7 @@ public class javaAdvancedArrays2 {
     }
     */
 
+    /* 
     //Maximum Subarray Sum (kadane's algorithm)
     public static void kadanes(int numbers[]){
             int currentSum = 0;
@@ -73,5 +74,34 @@ public class javaAdvancedArrays2 {
     public static void main(String[] args) {
         int numbers[] = {-1,-4,2,5,-6,7,4,-2};
         kadanes(numbers);
+    }
+    */
+
+    //Trapping Rainwater
+    public static int trappingRainwater(int height[]){
+        int n = height.length;
+
+        int left_max[] = new int [n];
+        left_max[0] = height[0];
+        for(int i = 1; i < n; i++){
+            left_max[i] = Math.max(height[i], left_max[i-1]);
+        }
+        
+        int right_max[] = new int [n];
+        right_max[n-1] = height[n-1];
+        for(int i = n-2; i >= 0; i--){
+            right_max[i] = Math.max(height[i], right_max[i+1]);
+        }
+
+        int trappedWater = 0;
+        for(int i = 0; i < n; i++){
+            int waterLevel = Math.min(left_max[i], right_max[i]);
+            trappedWater += waterLevel - height[i];
+        }
+        return trappedWater;
+    }
+    public static void main(String[] args) {
+        int height[] = {4, 2, 0, 6, 3, 2, 5};
+        System.out.println(trappingRainwater(height));
     }
 }
