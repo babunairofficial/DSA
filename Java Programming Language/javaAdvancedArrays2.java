db549169f11464b8711c6cb0a284271c05b5822d
Babu Nair <babunairofficial@gmail.com>
2024-10-11 20:52:56 +0530

    DSA - arrays part 2 - maximum subarray sum (kadane's algorith)

--- Java Programming Language/javaAdvancedArrays2.java
+++ Java Programming Language/javaAdvancedArrays2.java
@@ -25,6 +25,7 @@ public class javaAdvancedArrays2 {
     }
     */
     
+    /* 
     //Maximum Subarray Sum (Prefix sum approach)
     public static void subArraysSum(int numbers[]){
         int currentSum = 0;
@@ -53,5 +54,24 @@ public class javaAdvancedArrays2 {
     public static void main(String[] args) {
         int numbers[] = {0, -2, 4, -1, 8, 10};
         subArraysSum(numbers);
+    }
+    */
+
+    //Maximum Subarray Sum (kadane's algorithm)
+    public static void kadanes(int numbers[]){
+            int currentSum = 0;
+            int maxSum = Integer.MIN_VALUE;
+            for(int i = 0; i < numbers.length; i++){
+                currentSum += numbers[i];
+                if(currentSum < 0){
+                    currentSum = 0;
+                }
+                maxSum = Math.max(currentSum, maxSum);
+            }
+            System.out.println(maxSum);
+    }
+    public static void main(String[] args) {
+        int numbers[] = {-1,-4,2,5,-6,7,4,-2};
+        kadanes(numbers);
     }
 }
