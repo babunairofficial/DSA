e2a6241d96d39d84b3cc1df463cb985f29819b48
Babu Nair <babunairofficial@gmail.com>
2024-10-21 23:34:44 +0530

    DSA - sorting algorithms - insertion sort

--- Java Programming Language/javaAdvancedSorting.java
+++ Java Programming Language/javaAdvancedSorting.java
@@ -40,10 +40,25 @@ public class javaAdvancedSorting {
         }
     }
 
+    //insertion sort
+    public static void insertionSort(int arr[]){
+        for(int i = 1; i < arr.length; i++){
+            int curr = i;
+            int prev = i-1;
+            //finding the position to insert
+            while(prev >= 0 && arr[prev] > curr){ 
+                arr[prev+1] = arr[prev];
+                prev--;
+            }
+            //inserting
+            arr[prev + 1] = curr;
+        }
+    }
     public static void main(String[] args) {
         int arr[] = {9,7,2,8,3,6};
         // bubbleSort(arr);
-        selectionSort(arr);
+        // selectionSort(arr);
+        insertionSort(arr);
         printArr(arr);
     }
 }
