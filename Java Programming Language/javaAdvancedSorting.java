f510e21c8974520b9717828466ec54cf193f6357
Babu Nair <babunairofficial@gmail.com>
2024-10-23 10:13:53 +0530

    DSA - sorting algorithms - counting sort

--- Java Programming Language/javaAdvancedSorting.java
+++ Java Programming Language/javaAdvancedSorting.java
@@ -1,4 +1,4 @@
-import java.util.*;
+// import java.util.*;
 public class javaAdvancedSorting {
     /**
      * Sorts an array using the bubble sort algorithm.
@@ -55,13 +55,35 @@ public class javaAdvancedSorting {
             arr[prev + 1] = curr;
         }
     }
+
+    //counting sort
+    public static void countingSort(int arr[]){
+        int largest = Integer.MIN_VALUE;
+        for(int i = 0; i < arr.length; i++){
+            largest = Math.max(largest, arr[i]);
+        }
+        int count[] = new int[largest + 1];
+        for(int i = 0; i <arr.length; i++){
+            count[arr[i]]++;
+        }
+        //sorting
+        int j = 0;
+        for(int i = 0; i < count.length; i++){
+            while(count[i] > 0){
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
     public static void main(String[] args) {
-        int arr[] = {9,7,2,8,3,6};
+        int arr[] = {1,9,7,4,2,8,3,2,6};
         // bubbleSort(arr);
         // selectionSort(arr);
         // insertionSort(arr);
         // Arrays.sort(arr);
-        Arrays.sort(arr, 0, 3);
+        // Arrays.sort(arr, 0, 3);
+        countingSort(arr);
         printArr(arr);
     }
 }
