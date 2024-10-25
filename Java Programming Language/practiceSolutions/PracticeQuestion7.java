2c0ffcf910ba39bf73e3f76b73b1da125ca8321e
Babu Nair <babunairofficial@gmail.com>
2024-10-25 21:52:45 +0530

    DSA - practicequestion7

--- Java Programming Language/practiceSolutions/PracticeQuestion7.java
+++ Java Programming Language/practiceSolutions/PracticeQuestion7.java
@@ -41,6 +41,21 @@ public class PracticeQuestion7 {
         printArray(arr);
     }
 
+    //insertion sort
+    public static void insertionSort(int arr[]){
+        for(int i = 1; i < arr.length; i++){
+            int key = arr[i];
+            int j = i-1;
+            
+            while(j >= 0 && arr[j] < key){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+        printArray(arr);
+    }
+
     public static void main(String[] args) {
         int arr[] = {3, 6, 2, 1, 8, 7, 4, 5, 3, 1};
         
@@ -55,7 +70,7 @@ public class PracticeQuestion7 {
         System.out.print("print sorted array: ");
         //sorting method call
         // bubbleSort(arr);
-        selectionSort(arr);
-        
+        // selectionSort(arr);
+        insertionSort(arr);
     }
 }
