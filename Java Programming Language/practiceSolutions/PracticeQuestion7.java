1f58a2aa3a03845d8528434aa3757eb853f72e0a
Babu Nair <babunairofficial@gmail.com>
2024-10-26 07:17:16 +0530

    DSA - practicequestion7

--- Java Programming Language/practiceSolutions/PracticeQuestion7.java
+++ Java Programming Language/practiceSolutions/PracticeQuestion7.java
@@ -56,6 +56,33 @@ public class PracticeQuestion7 {
         printArray(arr);
     }
 
+    //counting sort
+    public static void countingSort(int arr[]){
+
+        // Find largest element
+        int largest = Integer.MIN_VALUE;
+        for(int i = 0; i < arr.length; i++) {
+            largest = Math.max(largest, arr[i]);
+        }
+
+        // Create count array
+        int count[] = new int[largest + 1];
+        for(int i = 0; i < arr.length; i++) {
+        count[arr[i]]++;
+        }
+
+        // Sorting in descending order
+        int j = 0;
+        for(int i = count.length-1; i >= 0; i--) {  // Start from the largest number
+            while(count[i] > 0) {
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+        printArray(arr);
+    }
+
     public static void main(String[] args) {
         int arr[] = {3, 6, 2, 1, 8, 7, 4, 5, 3, 1};
         
@@ -71,6 +98,7 @@ public class PracticeQuestion7 {
         //sorting method call
         // bubbleSort(arr);
         // selectionSort(arr);
-        insertionSort(arr);
+        // insertionSort(arr);
+        countingSort(arr);
     }
 }
