10a21af7516e475a3bf49446a30447b3ef972400
Babu Nair <babunairofficial@gmail.com>
2025-11-21 19:10:49 +0530

    DSA - practicequestion8

--- Java Programming Language/practiceSolutions/PracticeQuestion8.java
+++ Java Programming Language/practiceSolutions/PracticeQuestion8.java
@@ -1,5 +1,8 @@
 public class PracticeQuestion8 {
     public static void main(String[] args) {
+
+        /* 
+        // Question 1
         int[][] array = { {4, 7, 8}, {8, 8, 7} };
 
         int countOf7 = 0;
@@ -11,5 +14,17 @@ public class PracticeQuestion8 {
             }
         }
         System.out.println( "count of 7 is : " + countOf7);
+        */
+
+        // Question 2
+        int[][] nums = { {1,4,9}, {11,4,3}, {2,2,3} };
+        int sum = 0;
+
+        // sum of 2nd row elements
+        for(int j = 0; j < nums[0].length; j++) {
+            sum += nums[1][j];
+        }
+
+        System.out.println("sum is : " + sum);
     }    
 }
