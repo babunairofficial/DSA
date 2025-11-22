3c64f1e9308abffbbebc73eab116875708a83084
Babu Nair <babunairofficial@gmail.com>
2025-11-22 20:43:04 +0530

    DSA - practicequestion8

--- Java Programming Language/practiceSolutions/PracticeQuestion8.java
+++ Java Programming Language/practiceSolutions/PracticeQuestion8.java
@@ -16,6 +16,7 @@ public class PracticeQuestion8 {
         System.out.println( "count of 7 is : " + countOf7);
         */
 
+        /* 
         // Question 2
         int[][] nums = { {1,4,9}, {11,4,3}, {2,2,3} };
         int sum = 0;
@@ -26,5 +27,36 @@ public class PracticeQuestion8 {
         }
 
         System.out.println("sum is : " + sum);
-    }    
+        */
+
+
+        //Question 3
+        int row = 2;
+        int col = 3;
+
+        int [][] matrix = { {2, 3, 7}, {5, 6, 7} };
+
+        //Display original matrix
+        printMatrix(matrix);
+
+        //Transpose the matrix 
+        int[][] transpose = new int[col][row];
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j < col; j++) {
+                transpose[j][i] = matrix[i][j];
+            }
+        }
+
+        //print the transposed matrix
+        printMatrix(transpose);
+    }
+    public static void printMatrix(int[][] matrix) {
+        System.out.println("The matix is : ");
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + "   ");
+            }
+            System.out.println();
+        }
+    }
 }
