130eac55d374c75d9461470c878a06453d5c528b
Babu Nair <babunairofficial@gmail.com>
2025-12-01 22:28:09 +0530

    DSA - Strings - to uppercase

--- Java Programming Language/javaAdvancedStrings.java
+++ Java Programming Language/javaAdvancedStrings.java
@@ -13,6 +13,23 @@ public class javaAdvancedStrings {
         }
         return substr;
     }
+    public static String toUpperCase(String str) {
+            StringBuilder sb = new StringBuilder("");
+
+            char ch = Character.toUpperCase(str.charAt(0));
+            sb.append(ch);
+
+            for(int i = 1; i < str.length(); i++) {
+                if(str.charAt(i) == ' ' && i < str.length()-1) {
+                    sb.append(str.charAt(i));
+                    i++;
+                    sb.append(Character.toUpperCase(str.charAt(i)));
+                } else {
+                    sb.append(str.charAt(i));
+                }
+            }
+            return sb.toString();
+        }
     public static void main(String args[]) {
         char arr[] ={'a', 'b', 'c', 'd'};
         String str = "abcd";
@@ -83,5 +100,11 @@ public class javaAdvancedStrings {
         }
 
         System.out.println(sb.length());
+
+
+        //Word to uppercase
+        
+        String lowerstr = "hi, i am ramesh";
+        System.out.println(toUpperCase(lowerstr));
     }
 }
