60767cd8417eb7a40bbb5b81f2a88a99f0bed510
Babu Nair <babunairofficial@gmail.com>
2025-12-02 12:46:33 +0530

    DSA - practicequestion9

--- Java Programming Language/practiceSolutions/PracticeQuestion9.java
+++ Java Programming Language/practiceSolutions/PracticeQuestion9.java
@@ -1,9 +1,13 @@
 package practiceSolutions;
 
-import java.util.Scanner;
+import java.util.Arrays;
+// import java.util.Scanner;
 
 public class PracticeQuestion9 {
     public static void main(String[] args) {
+
+        /* 
+        //Q1
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
         int count = 0;
@@ -15,5 +19,31 @@ public class PracticeQuestion9 {
             }
         }
         System.out.println("count of vowels is :" + count);
+        */
+
+        //Q2
+        String str1 = "taste";
+        String str2 = "state";
+
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+
+        if(str1.length() == str2.length()) {
+            char[] str1charArray = str1.toCharArray();
+            char[] str2charArray = str2.toCharArray();
+
+            Arrays.sort(str1charArray);
+            Arrays.sort(str2charArray);
+
+            boolean result = Arrays.equals(str1charArray, str2charArray);
+
+            if(result) {
+                System.out.println(str1 + " and " + str2 + " are anagrams of each other.");
+            } else {
+                System.out.println(str1 + " and " + str2 + " are not anagrams of each other.");
+            }
+        } else {
+            System.out.println(str1 + " and " + str2 + " are not anagrams of each other.");
+        }
     }
 }
