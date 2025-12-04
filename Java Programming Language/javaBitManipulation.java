5786f3ac8053d3ab0d3ae237e5f4c602e8daddfe
Babu Nair <babunairofficial@gmail.com>
2025-12-04 22:20:26 +0530

    DSA - odd or even with bitMask

--- Java Programming Language/javaBitManipulation.java
+++ Java Programming Language/javaBitManipulation.java
@@ -1,4 +1,15 @@
 public class javaBitManipulation {
+
+    public static void oddOrEven(int n) {
+        int bitMask = 1;
+        if((n & bitMask) == 0) {
+            //even
+            System.out.println("even number");
+        } else {
+            //odd
+            System.out.println("odd number");
+        }
+    }
     public static void main(String[] args) {
 
         //Binary Left Shift
@@ -6,5 +17,10 @@ public class javaBitManipulation {
 
         //Binary Right Shift
         System.out.println((6>>1));
+
+        //check odd or even
+        oddOrEven(5);
+        oddOrEven(17);
+        oddOrEven(24);
     }
 }
