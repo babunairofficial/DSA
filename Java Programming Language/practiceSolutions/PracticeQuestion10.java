181c265b74445bc4e08a21305db241c6b7859d70
Babu Nair <babunairofficial@gmail.com>
2025-12-10 06:51:37 +0530

    practice question 10

--- Java Programming Language/practiceSolutions/PracticeQuestion10.java
+++ Java Programming Language/practiceSolutions/PracticeQuestion10.java
@@ -2,6 +2,9 @@ package practiceSolutions;
 
 public class PracticeQuestion10 {
     public static void main(String[] args) {
+
+        /* 
+        //Q2
         int x = 3, y = 4;
         System.out.println("Before swap: x = " + x + " and y = " + y);
         //swap using xor
@@ -9,5 +12,14 @@ public class PracticeQuestion10 {
         y = x ^ y;
         x = x ^ y;
         System.out.println("After swap: x = " + x + " and y = " + y);
+        */
+
+        //Q3
+        int x = 6;
+        System.out.println(x + " + " + 1 + " is " + -~x);
+        x = -4;
+        System.out.println(x + " + " + 1 + " is " + -~x);
+        x = 0;
+        System.out.println(x + " + " + 1 + " is " + -~x);
     }
 }
