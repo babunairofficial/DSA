f0ae03daecae8f53cfbe860b020012e84348188b
Babu Nair <babunairofficial@gmail.com>
2025-12-15 06:50:49 +0530

    DSA - OOPS - solved questions

---
+++ Java Programming Language/OopsSolvedQuestions.java
@@ -0,0 +1,13 @@
+public class OopsSolvedQuestions {
+    public static void main(String[] args) {
+        //Solved Practice Question 1
+        Student s = new Student();
+        s.name = "Alpha";
+        System.out.println(s.name);
+    }    
+}
+
+class Student {
+    String name;
+    int marks;
+}
