1fa26aeed58731dbbf932a2f32aa5a30d032a763
Babu Nair <babunairofficial@gmail.com>
2025-12-15 06:20:50 +0530

    DSA - OOPS - super keyword

--- Java Programming Language/OOPSKeyword.java
+++ Java Programming Language/OOPSKeyword.java
@@ -1,4 +1,7 @@
+//Static keyword
+/* 
 public class OOPSKeyword {
+
     public static void main(String[] args) {
         Student s1 = new Student();
         s1.schoolName = "BVB";
@@ -19,5 +22,30 @@ class Student {
     }
     String getName() {
         return this.name;
+    }
+}
+
+*/
+
+//Super Keyword
+public class OOPSKeyword {
+    public static void main(String[] args) {
+        Horse h = new Horse();
+        System.out.println(h.color);
+        
+    }
+}
+
+class Animal {
+    String color;
+    Animal() {
+        System.out.println("animal constructor is called");
+    }
+}
+
+class Horse extends Animal {
+    Horse() {
+        super.color = "brown";
+        System.out.println("horse constructor is called");
     }
 }
