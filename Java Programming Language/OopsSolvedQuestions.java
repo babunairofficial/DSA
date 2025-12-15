7976c1c18dbd0324ee7814c1b4a08530df3165e9
Babu Nair <babunairofficial@gmail.com>
2025-12-15 07:00:04 +0530

    DSA - OOPS - solved questions

--- Java Programming Language/OopsSolvedQuestions.java
+++ Java Programming Language/OopsSolvedQuestions.java
@@ -1,13 +1,32 @@
 public class OopsSolvedQuestions {
     public static void main(String[] args) {
         //Solved Practice Question 1
+        /* 
         Student s = new Student();
         s.name = "Alpha";
         System.out.println(s.name);
+        */
+
+        //Solved Practice Question 2
+        Person p = new Person();
+        p.name = "peter";
+        p.weight = 50;
+        System.out.println(p.name);
+        System.out.println(p.weight);
     }    
 }
-
+/* 
 class Student {
     String name;
     int marks;
+}
+*/
+
+class Person {
+    String name;
+    int weight;
+}
+class Student extends Person{
+    int rollNumber;
+    String schoolName;
 }
