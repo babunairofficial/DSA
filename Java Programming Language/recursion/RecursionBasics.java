ea8c363ec3bdd316f0eb3c6242544a351110f2db
Babu Nair <babunairofficial@gmail.com>
2025-12-17 20:26:12 +0530

    DSA - Recursion

--- Java Programming Language/recursion/RecursionBasics.java
+++ Java Programming Language/recursion/RecursionBasics.java
@@ -46,12 +46,27 @@ public class RecursionBasics {
         int fn = fib1 + fib2;
         return fn;
     }
+
+    //checks if an array is sorted or not
+    public static boolean isSorted(int arr[], int i) {
+        if(i == arr.length-1) {
+            return true;
+        }
+
+        if(arr[i] > arr[i+1]) {
+            return false;
+        }
+
+        return isSorted(arr, i+1);
+    }
     public static void main(String[] args) {
-        int n = 25;
+        // int n = 25;
+        int arr[] = {5, 6, 9};
         // printDec(n);
         // printInc(n);
         // System.out.println(fact(n));
         // System.out.println(sum(n));
-        System.out.println(fib(n));
+        // System.out.println(fib(n));
+        System.out.println(isSorted(arr, 0));
     }
 }
