9d0202404ff94308faba82cea5eb979db649243e
Babu Nair <babunairofficial@gmail.com>
2025-12-17 16:56:51 +0530

    DSA - Recursion

--- Java Programming Language/recursion/RecursionBasics.java
+++ Java Programming Language/recursion/RecursionBasics.java
@@ -9,8 +9,18 @@ public class RecursionBasics {
         System.out.print(n + " ");
         printDec(n - 1);
     }
+
+    public static void printInc(int n) {
+        if(n == 1) {
+            System.out.print(n + " ");
+            return;
+        }
+        printInc(n-1);
+        System.out.print(n + " ");
+    }
     public static void main(String[] args) {
         int n = 10;
-        printDec(n);
+        // printDec(n);
+        printInc(n);
     }
 }
