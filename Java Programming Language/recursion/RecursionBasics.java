3ce43b4181adbb5ece716e31a651516b1ac266dc
Babu Nair <babunairofficial@gmail.com>
2025-12-17 17:45:07 +0530

    DSA - Recursion

--- Java Programming Language/recursion/RecursionBasics.java
+++ Java Programming Language/recursion/RecursionBasics.java
@@ -27,10 +27,19 @@ public class RecursionBasics {
         int fn = n * fact(n-1);
         return fn;
     }
+
+    public static int sum(int n) {
+        if(n == 1) {
+            return 1;
+        }
+        int s = n + sum(n-1);
+        return s;
+    }
     public static void main(String[] args) {
         int n = 5;
         // printDec(n);
         // printInc(n);
-        System.out.println(fact(n));
+        // System.out.println(fact(n));
+        System.out.println(sum(n));
     }
 }
