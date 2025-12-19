ae9025efff68a85307c45c0c1dba882916c15dd2
Babu Nair <babunairofficial@gmail.com>
2025-12-19 20:26:14 +0530

    DSA - Recursion 2 - friends pairing

--- Java Programming Language/recursion/Recursion.java
+++ Java Programming Language/recursion/Recursion.java
@@ -36,10 +36,32 @@ public class Recursion {
         }
 
     }
+
+    public static int friendsPairing(int n) {
+        if(n == 1 || n ==2) {
+            return n;
+        }
+
+        //single 
+        int fnm1 = friendsPairing(n-1);
+
+        //pair
+        int fnm2 = friendsPairing(n-2);
+        int pairWays = (n-1) * fnm2;
+
+        //total ways
+        int totalWays = fnm1 + pairWays;
+        return totalWays;
+
+        // quicker way for friends pairing question
+        // return friendsPairing(n-1) + (n-1) * friendsPairing(n-2);
+    }
     public static void main(String[] args) {
         // System.out.println(tilingProblem(4));
 
-        String str = "indianinstituteoftechnology";
-        removeDuplicates(str, 0, new StringBuilder(""), new boolean[26]);
+        // String str = "indianinstituteoftechnology";
+        // removeDuplicates(str, 0, new StringBuilder(""), new boolean[26]);
+
+        System.out.println(friendsPairing(3));
     }
 }
