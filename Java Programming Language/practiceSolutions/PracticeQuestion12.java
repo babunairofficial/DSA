936e9ae029998eee46d3a3b4ddecebfa8f1de4c5
Babu Nair <babunairofficial@gmail.com>
2025-12-23 22:11:12 +0530

    DSA - practicequestion12

--- Java Programming Language/practiceSolutions/PracticeQuestion12.java
+++ Java Programming Language/practiceSolutions/PracticeQuestion12.java
@@ -2,6 +2,7 @@
 package practiceSolutions;
 
 public class PracticeQuestion12 {
+    //question 1
     public static void allOccurences(int arr[], int key, int i) {
         if(i == arr.length) {
             return;
@@ -11,10 +12,27 @@ public class PracticeQuestion12 {
         }
         allOccurences(arr, key, i+1);
     }
+
+    //question 2
+    
+    static String digits[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+    public static void printDigits(int number) {
+        if(number == 0) {
+            return;
+        }
+
+        int lastDigit = number%10;
+        printDigits(number/10);
+        System.out.print(digits[lastDigit] + " ");
+    }
+    
+
     public static void main(String[] args) {
         int arr[] = {3, 2, 4, 5, 6, 2, 7, 2, 2};
         int key = 2;
         allOccurences(arr, key, 0);
         System.out.println();
+        printDigits(1234);
     }
 }
