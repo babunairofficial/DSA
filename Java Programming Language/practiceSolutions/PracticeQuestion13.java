28ffdba8c532e181e06833dd2ae086c7454548b0
Babu Nair <babunairofficial@gmail.com>
2025-12-30 21:52:13 +0530

    DSA - practicequestion13

--- Java Programming Language/practiceSolutions/PracticeQuestion13.java
+++ Java Programming Language/practiceSolutions/PracticeQuestion13.java
@@ -119,6 +119,54 @@ public class PracticeQuestion13 {
         return majorityElementRec(nums, 0, nums.length-1);
     }
 
+    //question 3
+    public static int merge(int arr[], int left, int mid, int right) {
+        int i = left, j = mid, k = 0;
+        int invCount = 0;
+        int temp[] = new int[(right - left + 1)];
+        while ((i < mid) && (j <= right)) {
+            if (arr[i] <= arr[j]) {
+                temp[k] = arr[i];
+                k++;
+                i++;
+            }
+            else {
+                temp[k] = arr[j];
+                invCount += (mid - i);
+                k++;
+                j++;
+            }
+        }
+        while (i < mid) {
+            temp[k] = arr[i];
+            k++;
+            i++;
+        }
+        while (j <= right) {
+            temp[k] = arr[j];
+            k++;
+            j++;
+        }
+        for (i = left, k = 0; i <= right; i++, k++) {
+            arr[i] = temp[k];
+        }
+        return invCount;
+    }
+    private static int mergeSort(int arr[], int left, int right) {
+        int invCount = 0;
+        if (right > left) {
+            int mid = (right + left) / 2;
+            invCount = mergeSort(arr, left, mid);
+            invCount += mergeSort(arr, mid + 1, right);
+            invCount += merge(arr, left, mid + 1, right);
+        }
+        return invCount;
+    }
+    public static int getInversions(int arr[]) {
+        int n = arr.length;
+        return mergeSort(arr, 0, n - 1);
+    }
+
     public static void main(String[] args) {
         // String[] arr = { "sun", "earth", "mars", "mercury" };
         // String[] a = mergeSort(arr, 0, arr.length - 1);
@@ -131,5 +179,8 @@ public class PracticeQuestion13 {
 
         int nums[] = {2,2,1,1,1,2,2};
         System.out.println(majorityElement(nums));
+
+        int arr[] = {1, 20, 6, 4, 5};
+        System.out.println("Inversion Count = " + getInversions(arr));
     }
 }
